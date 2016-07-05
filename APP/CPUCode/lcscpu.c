abc84dd00563a931f9201e7cf5761b67b68a7d50
Uros Cibej <uros.cibej@fri.uni-lj.si>
2016-07-05 11:33:41 +0200

    Corrected a bug in lcs_cpu_2bits

--- APP/CPUCode/lcscpu.c
+++ APP/CPUCode/lcscpu.c
@@ -35,21 +35,22 @@ int lcs_cpu_2bits(char* x, int lenx, char* y, int leny) {
 		for (int j = 0; j < leny / 4; j++) {
 			char y_t = y[j / 4];
 			yb2[0] = y_t & 0b11;
-			yb2[1] = y_t & 0b1100;
-			yb2[2] = y_t & 0b110000;
-			yb2[3] = y_t & 0b11000000;
+			yb2[1] = y_t & 0b1100>>2;
+			yb2[2] = y_t & 0b110000>>4;
+			yb2[3] = y_t & 0b11000000>>6;
 			for (int k = 0; k < 4; k++) {
 				if (yb2[k] == xb2) {
 					curr_zp = curr_lp + 1;
-					curr_lp = l[j];
-					l[j] = curr_zp;
+					curr_lp = l[j*4+k];
+					l[j*4+k] = curr_zp;
 				} else {
-					curr_lp = l[j];
-					curr_zp = l[j] > curr_zp ? l[j] : curr_zp;
-					l[j] = curr_zp;
+					curr_lp = l[j*4+k];
+					curr_zp = l[j*4+k] > curr_zp ? l[j*4+k] : curr_zp;
+					l[j*4+k] = curr_zp;
 				}
 			}
 		}
 	}
+
 	return l[leny - 1];
 }
